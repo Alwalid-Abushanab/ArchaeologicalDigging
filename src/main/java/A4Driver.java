b39b8681a40e404789f67d433750877f061d6788
Alwalid-Abushanab <aabushanab@upei.ca>
2022-03-29 22:30:55 -0300

    basic tests solved

--- src/main/java/A4Driver.java
+++ src/main/java/A4Driver.java
@@ -13,16 +13,82 @@ public class A4Driver {
 
     public static void main(String[] args) {
 
-        //replace all of this code with your solution
+        Node start = null;
 
-        System.out.println("enter anything followed by <CTRL>+D");
-        Bag<String> data = new Bag<>();
+        //replace all of this code with your solution
         Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        scan.nextLine();
+
+        ArrayList<String> bones = new ArrayList<>();
+        ArrayList<String> order = new ArrayList<>();
+
         while(scan.hasNextLine()) {
-            data.add(scan.nextLine());
+            String line = scan.nextLine();
+            if(line.contains("rests"))
+                order.add(line);
+            else
+                bones.add(line);
         }
-        for(var s : data ) {
-            System.out.println(s);
+
+        if(order.isEmpty())
+        {
+            System.out.println(bones.get(0));
+        }
+
+        while(!order.isEmpty())
+        {
+            String firstBone = order.get(0).substring(0,order.get(0).indexOf(" "));
+            String secondBone = order.get(0).substring(order.get(0).indexOf(" on ") + 4);
+
+            if(start == null)
+            {
+                start = new Node(firstBone);
+                start.next = new Node(secondBone);
+            }
+
+            order.remove(0);
+        }
+
+
+        if(start != null) {
+            Node i;
+            for (i = start.goToLast(start); i.hasNext(); i = i.next) {
+                System.out.println(i.bone);
+            }
+            System.out.println(i.bone);
+        }
+
+    }
+
+    static class Node implements Iterator
+    {
+        Node next;
+        Node previous;
+        String bone;
+
+        Node(String name)
+        {
+            bone = name;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public Object next() {
+            return next;
+        }
+
+        public Node goToLast(Node node){
+            if(node.previous != null)
+            {
+                return goToLast(node.previous);
+            }
+            else
+                return node;
         }
     }
 }
